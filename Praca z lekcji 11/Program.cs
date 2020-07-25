368a3114632f459c813f0ee43c950d740fa95798
m.krzysiak14@gmail.com <m.krzysiak14@gmail.com>
2020-07-25 21:31:53 +0200

    Add project files

---
+++ Praca z lekcji 11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praca_z_lekcji_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {   
+                Console.WriteLine("Uzytkowniku podaj liczbe a ja odpowiem czy jest parzysta czy może nie");
+                var UserNumber = int.Parse(Console.ReadLine());
+                    if ((UserNumber % 2) == 0)
+                    {
+                        Console.WriteLine($"Liczba {UserNumber} jest Parzysta");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Liczba {UserNumber} jest Nieparzysta");
+                    };
+
+            }
+            catch(Exception)
+            {
+               Console.WriteLine("Podane dane nie są tylko liczbami.\nPodaj liczbę następnym razem." );
+            }
+            finally 
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
